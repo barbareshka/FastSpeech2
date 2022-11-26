d298474663478e0d5141062fdd27298314d4f48f
barbareshka <119070110+barbareshka@users.noreply.github.com>
2022-11-26 14:36:51 +0300

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -351,3 +351,24 @@ finals = [
     "vn5",
 ]
 valid_symbols = initials + finals + ["rr"]
+
+_pad = "_"
+_punctuation = "!'(),.:;? "
+_special = "-"
+_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+_silences = ["@sp", "@spn", "@sil"]
+
+# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
+_arpabet = ["@" + s for s in cmudict.valid_symbols]
+_pinyin = ["@" + s for s in pinyin.valid_symbols]
+
+# Export all symbols:
+symbols = (
+    [_pad]
+    + list(_special)
+    + list(_punctuation)
+    + list(_letters)
+    + _arpabet
+    + _pinyin
+    + _silences
+)
